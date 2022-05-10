695ce4f82e71d3e61007ca7224e0c7b145751ebb
Caphile <47132589+Caphile@users.noreply.github.com>
2022-05-11 07:58:20 +0900

    Update setting.py

--- setting.py
+++ setting.py
@@ -4,11 +4,12 @@ import pygame as pg
 import common as c
 
 def setStart():
-    pg.init()
-
     pg.display.set_caption("설정")
     gsDis = pg.display.set_mode(c.winSize)
     gsDis.fill(c.BLACK)
+
+    exitBtn = pg.draw.rect(mainDis, c.GRAY, ((c.winWidth - btnWidth) / 2, c.winHeight / 2 + btnHeight * 6, 
+                                             btnWidth, btnHeight * 1.5), 1) # 종료
 
     running = True
     while running:
