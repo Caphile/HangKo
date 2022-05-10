5a3468757a5a2d2aa27c541f8425d4a0b1f33699
Caphile <47132589+Caphile@users.noreply.github.com>
2022-05-11 07:57:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,29 +3,39 @@ import os
 import pygame as pg
 
 import common as c
-import gameSelect, rank, setting
+import gameSelectUI, rankUI, settingUI
 
 currentPath = os.getcwd()
 currentPath = os.path.join(currentPath, 'HangKo')
 
 def mainStart():
+
     pg.init()
-
-    # 기본 틀
-    pg.font.SysFont("notosanscjkkr", 10)
-    pg.display.set_caption("프로그램 이름(미정)")
-    mainDis = pg.display.set_mode(c.winSize)
-    mainDis.fill(c.WHITE)
-
-    # 버튼 구성
-    playBtn = pg.draw.rect(mainDis, c.GRAY, (300, 20, 50, 50), 1) # 게임시작
-    rankBtn = pg.draw.rect(mainDis, c.GRAY, (300, 70, 50, 50), 1) # 랭킹
-    setBtn = pg.draw.rect(mainDis, c.GRAY, (300, 120, 50, 50), 1) # 설정
-    exitBtn = pg.draw.rect(mainDis, c.GRAY, (300, 170, 50, 50), 1) # 종료
 
     running = True
     while running:
-        pg.display.flip()   # 화면 갱신
+        # 기본 틀
+        pg.font.SysFont("notosanscjkkr", 10)
+        pg.display.set_caption("프로그램 이름(미정)")
+        mainDis = pg.display.set_mode(c.winSize)
+        mainDis.fill(c.WHITE)
+
+        # 버튼 구성
+        btnWidth = 350
+        btnHeight = 28
+
+        playBtn = pg.draw.rect(mainDis, c.GRAY, ((c.winWidth - btnWidth) / 2, c.winHeight / 2, 
+                                                 btnWidth, btnHeight * 1.5), 1) # 게임시작
+        playText = pg.t
+        rankBtn = pg.draw.rect(mainDis, c.GRAY, ((c.winWidth - btnWidth) / 2, c.winHeight / 2 + btnHeight * 2, 
+                                                 btnWidth, btnHeight * 1.5), 1) # 랭킹
+        setBtn = pg.draw.rect(mainDis, c.GRAY, ((c.winWidth - btnWidth) / 2, c.winHeight / 2 + btnHeight * 4, 
+                                                btnWidth, btnHeight * 1.5), 1) # 설정
+        exitBtn = pg.draw.rect(mainDis, c.GRAY, ((c.winWidth - btnWidth) / 2, c.winHeight / 2 + btnHeight * 6, 
+                                                 btnWidth, btnHeight * 1.5), 1) # 종료
+
+        pg.display.update()   # 화면 갱신
+
         for event in pg.event.get():
             # 프로그램 종료
             if event.type == pg.QUIT:
@@ -33,11 +43,12 @@ def mainStart():
             # 클릭 이벤트
             elif event.type == pg.MOUSEBUTTONDOWN:
                 if playBtn.collidepoint(event.pos):
-                    gameSelect.gameSelectStart()
+                    if gameSelectUI.gameSelectStart() == 1:
+                        running = False
                 elif rankBtn.collidepoint(event.pos):
-                    rank.rankStart()
+                    rankUI.rankStart()
                 elif setBtn.collidepoint(event.pos):
-                    setting.setStart()
+                    settingUI.setStart()
                 elif exitBtn.collidepoint(event.pos):
                     running = False
 
