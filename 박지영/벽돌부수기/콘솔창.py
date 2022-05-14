3242af90441cf64bf125c00d0c64cf7117c00214
jiyoungP00 <101079009+jiyoungP00@users.noreply.github.com>
2022-05-14 23:14:07 +0900

    Update 콘솔창.py

--- 박지영/벽돌부수기/콘솔창.py
+++ 박지영/벽돌부수기/콘솔창.py
@@ -1,13 +1,14 @@
 import pygame
 
-pygame.init()
+pygame.init()  
 
-backgraound = pygame.display.set_mode((480, 360))  # 가로, 세로
+backgraound = pygame.display.set_mode((500, 800))
 pygame.display.set_caption("team13")
 
 play = True
-while play:  # 콘솔창이 계속 지속되도록 하기위해서 while문을 씀
+while play:  
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             play = False
-pygame.quit()
+            
+pygame.quit()  
